c1962c5116d493ae20701d34a4de1647a5525ca6
gogolxdong <lxdong.007@gmaijl.com>
2022-08-23 18:52:46 +0800

    update webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -300,6 +300,13 @@ module.exports = function (webpackEnv) {
       ],
     },
     resolve: {
+      fallback: {
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "url": require.resolve("url/"),
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify")
+      },
       // This allows you to set a fallback for where webpack should look for modules.
       // We placed these paths second because we want `node_modules` to "win"
       // if there are any conflicts. This matches Node resolution mechanism.
